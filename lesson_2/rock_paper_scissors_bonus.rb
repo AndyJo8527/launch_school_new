bdf4acd7b2aff5b7af17591c323f21515e1a998d
Andy Sullivan <andrewjsullivan227@gmail.com>
2022-11-18 21:38:49 +0000

    update

--- lesson_2/rock_paper_scissors_bonus.rb
+++ lesson_2/rock_paper_scissors_bonus.rb
@@ -1,5 +1,4 @@
 # use single letters instead of full words
-# reset score after three wins
 require 'yaml'
 MESSAGES = YAML.load_file('messages.yml')
 
@@ -10,7 +9,7 @@ WINNING_COMBOS = {
   'paper' => ['spock', 'rock'],
   'scissors' => ['paper', 'lizard'],
   'lizard' => ['spock', 'paper'],
-  'spock' => ['scissors', 'rock'],
+  'spock' => ['scissors', 'rock']
 }
 
 SCORE = { player: 0, computer: 0, ties: 0 }
@@ -41,6 +40,12 @@ def tie
   SCORE[:ties] += 1
 end
 
+def reset_score
+  SCORE.map do |key, _value|
+    SCORE[key] = 0
+  end
+end
+
 def update_score(first, second)
   if WINNING_COMBOS[first].include?(second)
     player_win
@@ -65,68 +70,72 @@ end
 
 prompt(MESSAGES['welcome'])
 prompt(MESSAGES['name_request'])
-name = gets.chomp().to_s()
+name = gets.chomp.to_s.capitalize
 valid_name(name)
 prompt("Welcome #{name}, let's go!")
 
 sleep(1)
 system('clear')
 
-prompt(MESSAGES['rules'])
-sleep(3)
-system('clear')
-
 loop do
-  choice = ''
+  prompt(MESSAGES['rules'])
+  sleep(3)
+  system('clear')
 
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets().chomp()
-    
-    if VALID_CHOICES.include?(choice)
+    choice = ''
+
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt(MESSAGES['valid_choice'])
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample()
+
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+    sleep(1)
+
+    win?(choice, computer_choice)
+    update_score(choice, computer_choice)
+
+    sleep(2)
+    system('clear')
+
+    if SCORE[:player] == 3
+      system('clear')
+      puts "--You won!--"
+      sleep(2)
+      break
+    elsif SCORE[:computer] == 3
+      system('clear')
+      puts "--Computer won!--"
+      sleep(2)
       break
     else
-      prompt(MESSAGES['valid_choice'])
+      SCORE.each do |participant, wins|
+        puts "#{participant.capitalize}: #{wins} wins"
+      end
+      puts ' '
+      next
     end
   end
 
-  computer_choice = VALID_CHOICES.sample()
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-  sleep(1)
-
-  win?(choice, computer_choice)
-  update_score(choice, computer_choice)
-
-  SCORE.each do |participant, wins|
-    puts "#{participant.capitalize}: #{wins} wins"
-  end
-
-  sleep(2)
-  system('clear')
-
-  if SCORE[:player] == 3
-    system('clear')
-    puts "--You won!--"
-    sleep(2)
-    break
-  elsif SCORE[:computer] == 3
-    system('clear')
-    puts "--Computer won!--"
-    sleep(2)
-    break
-  else
-    next
-  end
-end
-
-=begin
-This is where I would allow the player to restart the game
   prompt(MESSAGES['again'])
-  answer = gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer = gets.chomp.to_s
+  if answer.downcase == 'y' || answer.downcase == 'yes'
+    reset_score
+    system('clear')
+    next
+  else
+    break
+  end
 end
-=end
 
+system('clear')
 prompt("Thank you for playing #{name}. Good bye!")
-sleep(5)
+sleep(4)
